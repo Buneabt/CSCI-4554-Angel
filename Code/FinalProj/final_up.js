9ce5ff0833b257828ce0c2c3240268bf6b7ff087
Buneabt <buneabt@yahoo.com>
2026-04-08 12:57:05 -0400

    updates

--- Code/FinalProj/final_up.js
+++ Code/FinalProj/final_up.js
@@ -2,86 +2,121 @@ var finalProj = function() {
 
     "use strict";
     
+ 
     var canvas;
     var gl;
-    var ground_verts = 0; 
-    
-    var numPositions = 66;
-    
+ 
     var positionsArray = [];
     var colorsArray = [];
-    // Edits
+
+    var numPositions; 
+ 
+    var platformPositionsArray = []; // separate — platform only
+ 
+    // Render has to see our two programs now since that trains gonna be moving
+    // ask for my crude example (its very crude) where I worked with a moving wire train 
+    var platformProgram;
+    var program;
+
+    var trainmoving;
+    var movingcounter = 0;
+    var train_z = 0;
+ 
+    // The two different areas, we need to split up what will be affected by lighting
+    var main; // Not affected
+    var platform; //Has the lighting streak on it 
+
+    var vBuffer;
+    var cBuffer;
 
 
     //https://www.alamy.com/stock-photo/metro-rail-rapid-transit-washington-dc.html image of reference
 
     
-    var vertices = [ //This is the one thing that will update will move into render when done
-        // Front face (z = front) — flat trapezoid, nearly rectangular
-        vec4(-0.5 ,-1.0, 0.5, 1.0),  // 0 front bl
-        vec4(-0.4, -0.25, 0.5, 1.0),  // 1 front tl  (very slight inward taper)
-        vec4( 0.4, -0.25, 0.5, 1.0),  // 2 front tr
-        vec4( 0.5, -1.0, 0.5, 1.0),  // 3 front br
-    
-        // Back face (z = back)
-        vec4(-0.5,  -1.0,  -3.5,  1.0),  // 4 back bl
-        vec4(-0.4,  -0.25, -3.5,  1.0),  // 5 back tl
-        vec4( 0.4,  -0.25, -3.5,  1.0),  // 6 back tr
-        vec4( 0.5,  -1.0,  -3.5,  1.0),  // 7 back br
-    ];
+    function mytrainismoving() {
 
-    var vertexColors = [
-        vec4(0.0, 0.0, 0.0, 1.0),  // black
-        vec4(0.7, 0.3, 0.1, 1.0),  // Z + This was the red side, changed it to light gray to slightly match photo better
-        vec4(0.8, 0.8, .5, 1.0),  // X + yellow, tanish now
-        vec4(0.0, 1.0, 0.0, 1.0),  // green
-        vec4(0.0, 0.0, 1.0, 1.0),  // blue
-        vec4(.38, 0.36, .26, 1.0),  // This was the magenta side, changed it to light tan 
-        vec4(0.0, 1.0, 1.0, 1.0),  // cyan
-        vec4(1.0, 1.0, 1.0, 1.0),  // white
-    ];
+        // Our train is 1 car, 7.5 feet tall 
+        //(the wheels are 40 inches thus the 7.5 remaining is floor to ceiling)
+        //width is 10 ft 1+3⁄4 in (lets just say 10ft), ceiling width is not given
+
+        // the length of the car is 75 ft https://en.wikipedia.org/wiki/Washington_Metro_rolling_stock#7000-series
+        //These will be used for our dimensions
+        //Height of 0.75 thus train car length is x10 (7.5)
+        //width is 10/7.5 = 1.33, divide by 2 for both sides approx 0.66
+
+        var trainVert = [ //This is the one thing that will update will move into render when done
+            // Front face (z = front) — flat trapezoid, nearly rectangular
+            vec4(-0.66 ,-1.0, -5.5 - train_z, 1.0),  // 0 front bl
+            vec4(-0.5, -0.25, -5.5 - train_z, 1.0),  // 1 front tl  (very slight inward taper)
+            vec4( 0.5, -0.25, -5.5 - train_z, 1.0),  // 2 front tr
+            vec4( 0.66, -1.0, -5.5 - train_z, 1.0),  // 3 front br
+        
+            // Back face (z = back)
+            vec4(-0.66, -1.0, -13 - train_z, 1.0),  // 4 back bl
+            vec4(-0.5, -0.25, -13 - train_z, 1.0),  // 5 back tl
+            vec4( 0.5, -0.25, -13 - train_z, 1.0),  // 6 back tr
+            vec4( 0.66, -1.0, -13 - train_z, 1.0),  // 7 back br
+        ];
+
+        var vertexColors = [
+            vec4(0.0, 0.0, 0.0, 1.0),  // black
+            vec4(0.7, 0.3, 0.1, 1.0),  // Z + This was the red side, changed it to light gray to slightly match photo better
+            vec4(0.8, 0.8, .5, 1.0),  // X + yellow, tanish now
+            vec4(0.0, 1.0, 0.0, 1.0),  // green
+            vec4(0.0, 0.0, 1.0, 1.0),  // blue
+            vec4(.38, 0.36, .26, 1.0),  // This was the magenta side, changed it to light tan 
+            vec4(0.0, 1.0, 1.0, 1.0),  // cyan
+            vec4(1.0, 1.0, 1.0, 1.0),  // white
+        ];
+
+        function quad(a, b, c, d) {
+            positionsArray.push(trainVert[a]);
+            colorsArray.push(vertexColors[a]);
+            positionsArray.push(trainVert[b]);
+            colorsArray.push(vertexColors[a]);
+            positionsArray.push(trainVert[c]);
+            colorsArray.push(vertexColors[a]);
+            positionsArray.push(trainVert[a]);
+            colorsArray.push(vertexColors[a]);
+            positionsArray.push(trainVert[c]);
+            colorsArray.push(vertexColors[a]);
+            positionsArray.push(trainVert[d]);
+            colorsArray.push(vertexColors[a]);
+       }
+
+       function colorCube()
+       {
+           quad(1, 0, 3, 2);
+           quad(2, 3, 7, 6);
+           quad(3, 0, 4, 7);
+           quad(6, 5, 1, 2);
+           quad(4, 5, 6, 7);
+           quad(5, 4, 0, 1);
+       }
+
+       colorCube();
+    }
 
     // Our train platform
 
     var platform = [ // Large square that hits only the train edge
                        // Will imitate the train platform
-        vec4(0.7 ,-1, -2.5, 1.0),
-        vec4(0.7, -1, 10, 1.0),
+        vec4(0.85 ,-1, -2.5, 1.0),
+        vec4(0.85, -1, 10, 1.0),
         vec4(1.5, -1, 10, 1.0),  
-        vec4(1.5, -1, -2.5, 1)   
+        vec4(2.5, -1, -2.5, 1)   
     ];
 
     var platformEdge = [ // Large square that hits only the train edge
                        // Will imitate the train platform
-        vec4(0.5 ,-1, -2.5, 1.0),
-        vec4(0.5, -1, 10, 1.0),
-        vec4(0.7, -1, 10, 1.0),  
-        vec4(0.7, -1, -2.5, 1)   
+        vec4(0.66 ,-1, -2.5, 1.0),
+        vec4(0.66, -1, 10, 1.0),
+        vec4(0.85, -1, 10, 1.0),  
+        vec4(0.85, -1, -2.5, 1)   
     ];
-
-
-    // Verticies Back Wall
-
-    var platformbackwall = [ //cutout at the back of the train station see image for reference
-                       
-        //Front
-        vec4(0.7, -1, -2.5, 1.0),
-        vec4(3, -1, -2.5, 1.0),
-        vec4(3, 1, -2.5, 1.0),
-        vec4(-1.8, 1, -2.5, 1.0),
-        vec4(-1.8, -1, -2.5,  1.0),
-        vec4(-1,-1, -2.5, 1.0),
-        vec4(-1, 0, -2.5,  1.0),
-        vec4(0.7, 0, -2.5, 1.0)
-
-        //The default out of bounds color is black so its just hidden
-    ];
-
-
-
     
     var near = 0.3;
-    var far = 10.0;          // Had to make this bigger so the camera didn't get clipped 
+    var far = 20.0;          // Had to make this bigger so the camera didn't get clipped 
     var  fovy = 55.0;        // Field-of-view in Y direction angle (in degrees)
     var  aspect = 0.0;       // Viewport aspect ratio
     
@@ -91,120 +126,43 @@ var finalProj = function() {
     var at  = vec3(0.0, -0.6, 0.0);
     var up  = vec3(0.0, 1.0, 0.0);
 
-
-    
     init();
     
-    function quad(a, b, c, d) {
-         positionsArray.push(vertices[a]);
-         colorsArray.push(vertexColors[a]);
-         positionsArray.push(vertices[b]);
-         colorsArray.push(vertexColors[a]);
-         positionsArray.push(vertices[c]);
-         colorsArray.push(vertexColors[a]);
-         positionsArray.push(vertices[a]);
-         colorsArray.push(vertexColors[a]);
-         positionsArray.push(vertices[c]);
-         colorsArray.push(vertexColors[a]);
-         positionsArray.push(vertices[d]);
-         colorsArray.push(vertexColors[a]);
-    }
-
     // We need another drawing function here for the ground 
     // And it just needs to be a plane and not a cube
-    function platformMain(a, b, c, d) {
-        var maroon = vec4(0.5, 0.25, 0.15, 1.0);
+    function platformMain() {
         // One face (the brown), thus two triangles needed, thus three verticies. 6 total
-        positionsArray.push(platform[a]); 
-        colorsArray.push(maroon);
-        positionsArray.push(platform[b]); 
-        colorsArray.push(maroon);
-        positionsArray.push(platform[c]); 
-        colorsArray.push(maroon);
-        positionsArray.push(platform[a]); 
-        colorsArray.push(maroon);
-        positionsArray.push(platform[c]); 
-        colorsArray.push(maroon);
-        positionsArray.push(platform[d]); 
-        colorsArray.push(maroon);
+        platformPositionsArray.push(platform[0]); 
+        platformPositionsArray.push(platform[1]); 
+        platformPositionsArray.push(platform[2]); 
+        platformPositionsArray.push(platform[0]); 
+        platformPositionsArray.push(platform[2]); 
+        platformPositionsArray.push(platform[3]); 
     }
 
-    function platformEdingVerts(a, b, c, d) {
+    function platformEdingVerts() {
         var grey = vec4(0.6, 0.6, 0.6, 1);
         // One face (the brown), thus two triangles needed, thus three verticies. 6 total
-        positionsArray.push(platformEdge[a]); 
+        positionsArray.push(platformEdge[0]); 
         colorsArray.push(grey);
-        positionsArray.push(platformEdge[b]); 
+        positionsArray.push(platformEdge[1]); 
         colorsArray.push(grey);
-        positionsArray.push(platformEdge[c]); 
+        positionsArray.push(platformEdge[2]); 
         colorsArray.push(grey);
-        positionsArray.push(platformEdge[a]); 
+        positionsArray.push(platformEdge[0]); 
         colorsArray.push(grey);
-        positionsArray.push(platformEdge[c]); 
+        positionsArray.push(platformEdge[2]); 
         colorsArray.push(grey);
-        positionsArray.push(platformEdge[d]); 
+        positionsArray.push(platformEdge[3]); 
         colorsArray.push(grey);
-    }
-
-    function platformEndWallVerts() {
-        var darkergrey = vec4(0.5, 0.5, 0.5, 1);
-        // One face (the brown), thus two triangles needed, thus three verticies. 6 total
-        positionsArray.push(platformbackwall[0]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[1]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[2]); 
-        colorsArray.push(darkergrey);
-
-        positionsArray.push(platformbackwall[0]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[2]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[7]); 
-        colorsArray.push(darkergrey);
-
-        positionsArray.push(platformbackwall[2]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[3]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[7]); 
-        colorsArray.push(darkergrey);
-
-        positionsArray.push(platformbackwall[3]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[6]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[7]); 
-        colorsArray.push(darkergrey);
-
-        positionsArray.push(platformbackwall[3]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[4]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[6]); 
-        colorsArray.push(darkergrey);
-
-        positionsArray.push(platformbackwall[4]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[5]); 
-        colorsArray.push(darkergrey);
-        positionsArray.push(platformbackwall[6]); 
-        colorsArray.push(darkergrey);
-
-    }
-
-    
-    function colorCube()
-    {
-        quad(1, 0, 3, 2);
-        quad(2, 3, 7, 6);
-        quad(3, 0, 4, 7);
-        quad(6, 5, 1, 2);
-        quad(4, 5, 6, 7);
-        quad(5, 4, 0, 1);
     }
     
     function init() {
+        //Train event button
+        document.getElementById("button").onclick = function(event) {
+            trainmoving = true;
+        };
+
         canvas = document.getElementById("gl-canvas");
     
         gl = canvas.getContext('webgl2');
@@ -214,60 +172,135 @@ var finalProj = function() {
     
         aspect =  canvas.width/canvas.height;
     
-        gl.clearColor(0, 0, 1.0, 1.0); // Making this light blue like the sky in the photo
-    
+        gl.clearColor(0, 0, 0.0, 0.0); 
+        gl.clear(gl.COLOR_BUFFER_BIT);
         gl.enable(gl.DEPTH_TEST);
     
-        //
-        //  Load shaders and initialize attribute buffers
-        //
-        var program = initShaders(gl, "vertex-shader", "fragment-shader");
+
+        // First shader which is all the normal stuff
+        program = initShaders(gl, "vertex-shader", "fragment-shader");
         gl.useProgram(program);
     
-        colorCube();
-        //LOAD ALL OF OUR WALLS
-        platformMain(0,1,2,3);
-        platformEdingVerts(0,1,2,3);
-        platformEndWallVerts();
-    
-        var cBuffer = gl.createBuffer();
+        mytrainismoving();
+        platformMain();        
+
+        main = gl.createVertexArray();
+        gl.bindVertexArray(main);    
+
+        numPositions = positionsArray.length;
+ 
+
+ 
+        cBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
-    
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.DYNAMIC_DRAW);
         var colorLoc = gl.getAttribLocation(program, "aColor");
         gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(colorLoc);
-    
-        var vBuffer = gl.createBuffer();
+ 
+        vBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
-    
-        var positionLoc = gl.getAttribLocation( program, "aPosition");
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray),gl.DYNAMIC_DRAW);
+        var positionLoc = gl.getAttribLocation(program, "aPosition");
         gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(positionLoc);
-    
-        modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
+ 
+        gl.bindVertexArray(null);
+ 
+        modelViewMatrixLoc  = gl.getUniformLocation(program, "uModelViewMatrix");
         projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
-    
-    
+ 
+        // Second shader init
+        platformProgram = initShaders(gl, "platform-vertex-shader", "platform-fragment-shader");
+        gl.useProgram(platformProgram);
+ 
+        platform = gl.createVertexArray();
+        gl.bindVertexArray(platform);
+ 
+        var pvBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, pvBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(platformPositionsArray), gl.STATIC_DRAW);
+        var pPositionLoc = gl.getAttribLocation(platformProgram, "aPosition");
+        gl.vertexAttribPointer(pPositionLoc, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(pPositionLoc);
+ 
+        gl.bindVertexArray(null);
+ 
         render();
     }
     
     function render(){
-    
+
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
-        eye = vec3(1, -.5, 3.2); 
+        eye = vec3(1.5, -0.6, 5); 
         modelViewMatrix = lookAt(eye, at, up);
         projectionMatrix = perspective(fovy, aspect, near, far);
     
-        gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+        // Train movement logic
+        if (trainmoving) {
+            if (movingcounter <= 210) {
+                movingcounter++;
+                train_z -= 0.05; //slowing down
+            }
+            else if (movingcounter > 210 && movingcounter <= 250) {
+                movingcounter++;
+            }
+            else if (movingcounter > 250 && movingcounter <= 400) {
+                movingcounter++;
+                train_z -= 0.075; //Accelerating
+            }
+            else {
+                trainmoving = false;
+                movingcounter = 0;
+                train_z = 0;
+            }
+        }
+    
+        
+        positionsArray = [];
+        colorsArray = [];
+
+        mytrainismoving();
+        platformEdingVerts();
+
+
+        numPositions = positionsArray.length;
+    
+    
+        gl.bindVertexArray(main);
+    
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.DYNAMIC_DRAW);
+    
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.DYNAMIC_DRAW);
+    
+        gl.bindVertexArray(null);
+    
+        // Draw main scene
+        gl.useProgram(program);
+        gl.bindVertexArray(main);
+        gl.uniformMatrix4fv(modelViewMatrixLoc,  false, flatten(modelViewMatrix));
         gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
-    
         gl.drawArrays(gl.TRIANGLES, 0, numPositions);
-        requestAnimationFrame(render);
-    }
     
+        // Draw platform
+        gl.useProgram(platformProgram);
+        gl.bindVertexArray(platform);
+        gl.uniformMatrix4fv(
+            gl.getUniformLocation(platformProgram, "uModelViewMatrix"),
+            false, flatten(modelViewMatrix)
+        );
+        gl.uniformMatrix4fv(
+            gl.getUniformLocation(platformProgram, "uProjectionMatrix"),
+            false, flatten(projectionMatrix)
+        );
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    
+        setTimeout(function() {requestAnimationFrame(render);}, 1000/30);
     }
-    finalProj();
+
+    }
+finalProj();
     
---
+++ Code/HW4/Bunea_HW4.js
@@ -0,0 +1,241 @@
+    "use strict";
+
+    //Built this using the last hw assignment as well as the end of the lighting slide show/pickcube2
+    
+    var canvas;
+    var gl;
+    var ground_verts = 0; 
+    var program;
+    
+    var numPositions = 42;
+    
+    var positionsArray = [];
+    var colorsArray = [];
+
+    var is10am = true;
+
+    // Lighting Parameters
+
+    //Im having trouble here because it seems no matter what value I change my lighting and material too I get pure white light
+    // Im happy with that one corner being shaded because thats how that looked this morning where the only dark spot(ish) on the building was that
+
+    var lightingLoc;
+
+    var lightAmbient = vec4(0.8,0.7,0.7,1);
+    var lightDiffuse = vec4(.6,.6,.6,1);
+    var lightSpecular = vec4(0,0,0,1);
+
+    var lightPosition10am = vec4(-2,1,10,0); //Sun is so far away it doesnt matter
+    var lightPosition5pm = vec4(4,1,-2,0); //Basically the opposite on the x axis
+
+    //Material, its a glass wall, doesnt change
+    var materialAmbient = vec4(0.6, 0.8, 0.8, 1); //Base color
+    var materialDiffuse = vec4(0.02, 0.01, 0.01, 1); //changes light direction
+    var materialSpecular = vec4(0.1,0.1,0.1,1); //shiny (assuming windows from picture)
+    var materialShininess = 1;
+
+
+    //Normals
+    var normalsArray = [];
+
+    var x_gap = 1.86
+    var y_gap = 0.6
+    var z_gap = 1.04
+
+
+    var vertices = [
+        vec4(-0.93 , -1, -0.52 + z_gap, 1.0),  // 0 front bl
+        vec4(-0.93, -1 + y_gap, -0.52 + z_gap, 1.0),  // 1 front tl
+        vec4(-0.93 + x_gap, -1 + y_gap, -0.52 + z_gap, 1.0),  // 2 front tr
+        vec4(-0.93 + x_gap, -1, -0.52 + z_gap, 1.0),  // 3 front br
+        vec4(-0.93, -1, -0.52, 1.0),  // 4 back bl
+        vec4(-0.93 , -1 + y_gap, -0.52, 1.0),  // 5 back tl
+        vec4(-0.93 + x_gap, -1 + y_gap, -0.52 , 1.0),  // 6 back tr
+        vec4(-0.93 + x_gap, -1, -0.52, 1.0)   // 7 back br
+    ];
+
+    var verticies2 = [ // Can just make a large square that will get clipped out
+                       // So it will just look like our drawings
+        vec4(-.95, -1.,  .55, 1.0),  
+        vec4(3 + x_gap, -1.01,  1, 1.0), 
+        vec4(5 + x_gap, -2.01, -0.54, 1.0),  
+        vec4(-4.35, -2.01, -1, 1.0)   
+    ];
+    
+    
+    var near = 0.3;
+    var far = 10.0;          // Had to make this bigger so the camera didn't get clipped 
+    var  fovy = 65.0;        // Field-of-view in Y direction angle (in degrees)
+    var  aspect = 0.0;       // Viewport aspect ratio
+    
+    var modelViewMatrix, projectionMatrix;
+    var modelViewMatrixLoc, projectionMatrixLoc;
+    var eye;
+    var at  = vec3(0, 0.5, 0.0);
+    var up  = vec3(0.0, 1.0, 0.0);
+
+
+    
+    init();
+    //Our cube push normals
+    function quad(a, b, c, d) {
+
+        var t1 = subtract(vertices[b], vertices[a]);
+        var t2 = subtract(vertices[c], vertices[b]);
+        var normal = cross(t1, t2);
+        normal = normalize(normal);       
+
+        positionsArray.push(vertices[a]);
+        normalsArray.push(normal);
+        positionsArray.push(vertices[b]);
+        normalsArray.push(normal);
+        positionsArray.push(vertices[c]);
+        normalsArray.push(normal);
+        positionsArray.push(vertices[a]);
+        normalsArray.push(normal);
+        positionsArray.push(vertices[c]);
+        normalsArray.push(normal);
+        positionsArray.push(vertices[d]);
+        normalsArray.push(normal);
+        
+    }
+
+    // We need another drawing function here for the ground 
+    // And it just needs to be a plane and not a cube
+    function ground(a, b, c, d) {
+        var brown = vec4(0.35, 0.25, 0.15, 1.0);
+        ground_verts = 6;
+        // One face (the brown), thus two triangles needed, thus three verticies. 6 total
+        positionsArray.push(verticies2[a]); 
+        colorsArray.push(brown);
+        positionsArray.push(verticies2[b]); 
+        colorsArray.push(brown);
+        positionsArray.push(verticies2[c]); 
+        colorsArray.push(brown);
+        positionsArray.push(verticies2[a]); 
+        colorsArray.push(brown);
+        positionsArray.push(verticies2[c]); 
+        colorsArray.push(brown);
+        positionsArray.push(verticies2[d]); 
+        colorsArray.push(brown);
+        for (var i = 0; i < 6; i++) normalsArray.push(vec3(0,1,0));
+    }
+    
+    function colorCube()
+    {
+        quad(1, 0, 3, 2);
+        quad(2, 3, 7, 6);
+        quad(3, 0, 4, 7);
+        quad(6, 5, 1, 2);
+        quad(4, 5, 6, 7);
+        quad(5, 4, 0, 1);
+
+        for (var i = 0; i < 36; i++) colorsArray.push(vec4(0,0,0,1)); //Padding because 6 verts at the end are color, the first 36 are lighting thus pad
+    }
+    
+    function init() {
+        canvas = document.getElementById("gl-canvas");
+    
+        gl = canvas.getContext('webgl2');
+        if (!gl) alert("WebGL 2.0 isn't available");
+
+        document.getElementById("button10am").onclick = function(event) {
+            is10am = true;
+        };
+        document.getElementById("button5pm").onclick = function(event) {
+            is10am = false;
+        };
+    
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    
+        aspect =  canvas.width/canvas.height;
+    
+        gl.clearColor(0, 0.4, 1.0, 1.0); // Making this light blue like the sky in the photo
+    
+        gl.enable(gl.DEPTH_TEST);
+    
+        //
+        //  Load shaders and initialize attribute buffers
+        //
+        program = initShaders(gl, "vertex-shader", "fragment-shader");
+        gl.useProgram(program);
+    
+        colorCube();
+        ground(0,1,2,3);
+    
+        var nBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+        var normalLoc = gl.getAttribLocation(program, "aNormal");
+        gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(normalLoc);
+
+        var cBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW);
+    
+        var colorLoc = gl.getAttribLocation(program, "aColor");
+        gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(colorLoc);
+
+        var normalLoc = gl.getAttribLocation(program, "aNormal");
+        gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(normalLoc);
+    
+        var vBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+    
+        var positionLoc = gl.getAttribLocation( program, "aPosition");
+        gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(positionLoc);
+    
+        modelViewMatrixLoc = gl.getUniformLocation(program, "ModelViewMatrix");
+        projectionMatrixLoc = gl.getUniformLocation(program, "ProjectionMatrix");
+    
+        //Products
+        var ambientProduct = mult(lightAmbient,materialAmbient);
+        var diffuseProduct = mult(lightDiffuse,materialDiffuse);
+        var specularProduct = mult(lightSpecular,materialSpecular);
+    
+
+        gl.uniform4fv(gl.getUniformLocation(program, "AmbientProduct"), ambientProduct);
+        gl.uniform4fv(gl.getUniformLocation(program, "DiffuseProduct"), diffuseProduct );
+        gl.uniform4fv(gl.getUniformLocation(program, "SpecularProduct"), specularProduct );
+
+        gl.uniform1f(gl.getUniformLocation(program, "Shininess"), materialShininess);
+        
+        lightingLoc = gl.getUniformLocation(program, "Lighting");
+
+        render();
+    }
+    
+    function render(){
+    
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        if (is10am) {
+            gl.uniform4fv(gl.getUniformLocation(program, "LightPosition"), lightPosition10am);
+        } else {
+            gl.uniform4fv(gl.getUniformLocation(program, "LightPosition"), lightPosition5pm);
+        }
+    
+        eye = vec3(-2, -1.3, 2); // Since y is our up we want to be a bit negative on that front to get that low view (ground is -1 so we use -0.9)
+        modelViewMatrix = lookAt(eye, at, up);
+        projectionMatrix = perspective(fovy, aspect, near, far);
+    
+        gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+        gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+    
+        gl.uniform1i(lightingLoc, 1);
+        gl.drawArrays(gl.TRIANGLES, 0, 36); 
+
+        gl.uniform1i(lightingLoc, 0);
+        gl.drawArrays(gl.TRIANGLES, 36, 6);
+
+        requestAnimationFrame(render);
+    }
+    
+
+    
